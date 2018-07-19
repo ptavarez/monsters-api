cc0f9b9db9b36ced7d103b554804a5eefb791b8d
ptavarez16 <ptavarez16@yahoo.com>
2018-07-19 16:54:52 -0400

    Add node sql configuration

---
+++ bin/configuredb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+database="monstersdb"
+echo "Configurating database: $database"
+
+dropdb -U node_user monstersdb
+createdb -U node_user monstersdb
+
+psql -U node_user monstersdb < ./bin/sql/monsters.sql
+
+echo "database configured"
---
+++ bin/sql/monsters.sql
@@ -0,0 +1,36 @@
+CREATE TABLE monsters(
+  id serial,
+  name character varying(50),
+  personality character varying(50)
+);
+
+CREATE TABLE habitats(
+  id serial,
+  name character varying(50),
+  climate character varying(50),
+  temperatue int
+);
+
+CREATE TABLE lives(
+  id serial,
+  monster character varying(50),
+  habitat character varying(50)
+);
+
+INSERT INTO monsters(name, personality)
+VALUES
+('Dracula', 'Thirsty'),
+('Big Foot', 'Lazy'),
+('Zombie', 'Dumb');
+
+INSERT INTO habitats(name, climate, temperatue)
+VALUES
+('Cofin', 'Cold', '32'),
+('Forrest', 'Haunted', '70'),
+('Basement', 'Cool', '50');
+
+INSERT INTO lives(monster, habitat)
+VALUES
+('Dracula', 'Cofin'),
+('Big Foot', 'Forrest'),
+('Zombie', 'Basement');
